4d13b771f5c518d1d4af350d3d28afb216412004
Chase Rogness <chaserogness@gmail.com>
2025-12-03 18:42:51 -0600

    Accepting input

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -9,4 +10,12 @@ var _ = fmt.Print
 
 func main() {
 	fmt.Print("$ ")
+	var command string
+	_, err := fmt.Scanln(&command)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%v: command not found\n", command)
 }
